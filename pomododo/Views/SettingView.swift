82fd12bf9f0fbe56f69bb56f35d71b8bacb2b159
triet <triet.phm@gmail.com>
2020-03-29 18:40:46 +0700

    Remove focus-ring in TextField

--- pomododo/Views/SettingView.swift
+++ pomododo/Views/SettingView.swift
@@ -14,7 +14,6 @@ struct TextLabelStyle: ViewModifier {
             .font(Font
                 .custom("Arial Rounded MT Bold", size: 16)
                 .weight(.ultraLight))
-            .foregroundColor(.white)
     }
 }
 
@@ -24,30 +23,23 @@ struct SettingTextFieldNumberStyle: ViewModifier {
             .font(Font
                 .custom("Arial Rounded MT Bold", size: 14)
                 .weight(.light))
-            .foregroundColor(.white)
             .padding()
-            .border(Color.white, width: 1.5)
-            .cornerRadius(1.5)
-            .background(
-                Color(red: 37/255, green: 42/255, blue: 47/255))
-            .cornerRadius(1.5)
+            .border(Color.black, width: 1)
+            .cornerRadius(2)
     }
 }
+
 
 struct SettingView: View {
     @EnvironmentObject var settings: Setting
 
     var body: some View {
         ZStack {
-            Color(red: 37/255, green: 42/255, blue: 47/255)
-            .frame(height: 350)
-            
             VStack(spacing: 15) {
                 Text("Settings")
                     .font(Font
                         .custom("Arial Rounded MT Bold", size: 30)
                         .weight(.light))
-                    .foregroundColor(.white)
                     .padding(.bottom)
                 
 
@@ -75,14 +67,6 @@ struct SettingView: View {
                         .textFieldStyle(PlainTextFieldStyle())
                         .modifier(SettingTextFieldNumberStyle())
                 }
-                
-                Button(action: {
-                    print("\(self.settings.shortBreakLength)")
-                    print("\(self.settings.longBreakLength)")
-                    print("\(self.settings.sessionLength)")
-                }) {
-                Text(/*@START_MENU_TOKEN@*/"Button"/*@END_MENU_TOKEN@*/)
-                }
             }
                 //.frame(maxWidth: .infinity, maxHeight: .infinity)
                 .padding([.leading,.trailing])
@@ -96,5 +80,12 @@ struct settings_Previews: PreviewProvider {
     static var previews: some View {
         SettingView()
             .environmentObject(Setting())
+    }
+}
+
+extension NSTextField {
+    open override var focusRingType: NSFocusRingType {
+        get { .none }
+        set { }
     }
 }
